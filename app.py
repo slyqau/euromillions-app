917912a131abb9b0648bd7f6ac04501b9666d45d
slyqau <jalo650@gmail.com>
2025-06-19 17:59:48 +0200

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ st.set_page_config(page_title="EuroMillions Boost+ Advanced", layout="wide")
 @st.cache_data
 def load_data():
     try:
-        df = pd.read_csv("euromillions_full_1000.csv")
+        df = pd.read_csv("euromillions_full_from_second.csv")
         df["Date"] = pd.to_datetime(df["Date"])
         return df
     except:
